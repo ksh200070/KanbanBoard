2944e7e0feaf42ba35bd0ea7408ca161eecdcdfc
kim seung hyun <ksh20007@naver.com>
2025-02-04 13:38:21 +0900

    React router 설정, 스타일 초기화 reset.css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from '@components/Header/Header';
+import Main from '@pages/Main';
+import NotFound from '@pages/NotFount';
 
 function App() {
-  return <></>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       { find: '@', replacement: path.resolve(__dirname, 'src') },
       { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
       { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
     ],
   },
 });
