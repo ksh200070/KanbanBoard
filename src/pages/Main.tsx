f563c19cf68090a31544c06147ceee3516a8f7a6
kim seung hyun <ksh20007@naver.com>
2025-02-06 17:33:17 +0900

    feat : edit board title

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,13 +2,24 @@ import styles from './Main.module.scss';
 import rawData from '@data/data.json';
 import KanbanList from '@components/KanbanList/KanbanList';
 import { Board, KanbanListProps } from '@/types/global';
+import { useState } from 'react';
 
 export default function Main() {
-  const data: Board = rawData;
+  const [data, setData] = useState<Board>(rawData);
 
   return (
     <div className={styles.body}>
-      <h1 className={styles['board-title']}>{data.boardTitle}</h1>
+      <input
+        className={styles['board-title']}
+        defaultValue={data.boardTitle}
+        onChange={(e) =>
+          setData((prev) => {
+            const update = prev;
+            update['boardTitle'] = e.target.value;
+            return update;
+          })
+        }
+      />
       <section className={styles['list-container']}>
         {data.kanbanList.map((list: KanbanListProps) => (
           <KanbanList key={list.id} list={list} />
