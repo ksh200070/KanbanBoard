c30913d8a72632a81c4d5f62ef1718f89a150aa8
kim seung hyun <ksh20007@naver.com>
2025-02-09 01:10:55 +0900

    feat: add, edit, remove card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,35 @@
 import styles from './Card.module.scss';
-import { CardProps } from '@/types/global';
+import { CardProps, TagProps } from '@/types/global';
+import Tag from '@components/Tag/Tag';
+import DropdownMenu from '../DropdownMenu/DropdownMenu';
 
-export default function Card({ card }: { card: CardProps }) {
+export default function Card({
+  card,
+  onClick,
+  onDelete,
+}: {
+  card: CardProps;
+  onClick: () => void;
+  onDelete: () => void;
+}) {
+  const handleDropdownMenu = (selectedMenu: string) => {
+    if (selectedMenu === '카드 제거') {
+      onDelete();
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.tags}>
-        {card.tags.map((tag) => (
-          <div className={styles.tag}>관리자페이지</div>
-        ))}
+    <div className={styles.container} onClick={onClick}>
+      <div className={styles.header}>
+        <div className={styles.tags}>
+          {card.tags.map((tag: TagProps) => (
+            <Tag key={tag.name} name={tag.name} color={tag.color} />
+          ))}
+        </div>
+        <DropdownMenu
+          menuList={['카드 제거']}
+          onClick={(menu: string) => handleDropdownMenu(menu)}
+        ></DropdownMenu>
       </div>
       <span className={styles.content}>{card.content}</span>
     </div>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,20 +2,39 @@ import styles from './Main.module.scss';
 import { useState } from 'react';
 import Input from '@/components/Input/Input';
 import KanbanList from '@components/KanbanList/KanbanList';
-import { Board, KanbanListProps } from '@/types/global';
+import CardDetail from '@components/CardDetail/CardDetail';
+import { Board, CardProps, KanbanListProps } from '@/types/global';
 import rawData from '@data/data.json';
 
 export default function Main() {
   const [data, setData] = useState<Board>(rawData);
+  const [isCardSelected, setIsCardSelected] = useState(false);
+  const [selectedCard, setSelectedCard] = useState<CardProps | undefined>(undefined);
+  const [selectedCardListIdx, setSelectedCardListIdx] = useState<number | undefined>(undefined);
 
-  const updateListIndex = () => {
-    return data.kanbanList.length + 1;
+  const updateIndex = (target: 'list' | 'card') => {
+    if (target === 'list') {
+      return data.kanbanList.length + 1;
+    }
+
+    if (target === 'card' && selectedCardListIdx) {
+      const findList = data.kanbanList.find(
+        (list: KanbanListProps) => list.id === selectedCardListIdx,
+      );
+      if (findList) {
+        return findList.cards.length + 1;
+      }
+    }
   };
 
   const addListHandler = (inputValue: string) => {
     setData((prev: Board) => {
       const updated = { ...prev };
-      updated.kanbanList.push({ id: updateListIndex(), listTitle: inputValue, cards: [] });
+      updated.kanbanList.push({
+        id: updateIndex('list') as number,
+        listTitle: inputValue,
+        cards: [],
+      });
       return updated;
     });
   };
@@ -28,35 +47,127 @@ export default function Main() {
     });
   };
 
-  return (
-    <div className={styles.body}>
-      <input
-        className={styles['board-title']}
-        defaultValue={data.boardTitle}
-        onChange={(e) =>
-          setData((prev) => {
-            const update = prev;
-            update['boardTitle'] = e.target.value;
-            return update;
-          })
+  const deleteCardHandler = (listId: number, cardId: number) => {
+    setData((prev: Board) => {
+      const updated = { ...prev };
+      const updatedList = updated.kanbanList.map((list: KanbanListProps) => {
+        if (list.id === listId) {
+          return {
+            id: list.id,
+            listTitle: list.listTitle,
+            isFixed: list.isFixed,
+            cards: list.cards.filter((card: CardProps) => card.id !== cardId),
+          };
+        } else {
+          return list;
         }
-      />
-      <section className={styles['list-container']}>
-        {data.kanbanList.map((list: KanbanListProps) => (
-          <KanbanList
-            key={list.id}
-            list={list}
-            onDelete={(listId: number) => deleteListHandler(listId)}
-          />
-        ))}
+      });
+      return { id: updated.id, boardTitle: updated.boardTitle, kanbanList: updatedList };
+    });
+  };
 
-        <Input
-          onComplete={(inputValue) => addListHandler(inputValue)}
-          placeholder='Add another list'
-        >
-          +
-        </Input>
-      </section>
-    </div>
+  const clickCardHandler = (listId?: number, card?: CardProps) => {
+    setIsCardSelected((prev) => !prev);
+    setSelectedCard(card);
+    setSelectedCardListIdx(listId);
+  };
+
+  const closeCardDetailHandler = () => {
+    setIsCardSelected((prev) => !prev);
+    setSelectedCard(undefined);
+  };
+
+  const saveCardDetailHandler = (editCard: CardProps, isCreated?: boolean) => {
+    if (isCreated) {
+      setData((prev: Board) => {
+        const updated = { ...prev };
+        const updatedList = updated.kanbanList.map((list: KanbanListProps) => {
+          if (list.id === selectedCardListIdx) {
+            list.cards.push({
+              id: updateIndex('card') as number,
+              tags: editCard.tags,
+              content: editCard.content,
+            });
+          }
+
+          return list;
+        });
+
+        return {
+          id: updateIndex('card') as number,
+          boardTitle: updated.boardTitle,
+          kanbanList: updatedList,
+        };
+      });
+    } else {
+      if (!!selectedCard && !!selectedCardListIdx) {
+        setData((prev: Board) => {
+          const updated = { ...prev };
+          const updatedList = updated.kanbanList.map((list: KanbanListProps) => {
+            if (list.id === selectedCardListIdx) {
+              const updatedCards = list.cards.map((card: CardProps) =>
+                card.id === editCard.id ? editCard : card,
+              );
+
+              return {
+                id: list.id,
+                listTitle: list.listTitle,
+                cards: updatedCards,
+                isFixed: list.isFixed,
+              };
+            } else {
+              return list;
+            }
+          });
+
+          return { id: updated.id, boardTitle: updated.boardTitle, kanbanList: updatedList };
+        });
+      }
+    }
+    closeCardDetailHandler();
+  };
+
+  return (
+    <>
+      <div className={styles.body}>
+        <input
+          className={styles['board-title']}
+          defaultValue={data.boardTitle}
+          onChange={(e) =>
+            setData((prev) => {
+              const update = prev;
+              update['boardTitle'] = e.target.value;
+              return update;
+            })
+          }
+        />
+        <section className={styles['list-container']}>
+          {data.kanbanList.map((list: KanbanListProps) => (
+            <KanbanList
+              key={list.id}
+              list={list}
+              onDelete={(listId: number) => deleteListHandler(listId)}
+              onDeleteCard={(listId: number, cardId: number) => deleteCardHandler(listId, cardId)}
+              onClickCard={(listId?: number, card?: CardProps) => clickCardHandler(listId, card)}
+            />
+          ))}
+
+          <Input
+            onComplete={(inputValue) => addListHandler(inputValue)}
+            placeholder='Add another list'
+          >
+            +
+          </Input>
+        </section>
+      </div>
+
+      {!!isCardSelected && (
+        <CardDetail
+          card={selectedCard}
+          onSave={(card: CardProps, isCreated?: boolean) => saveCardDetailHandler(card, isCreated)}
+          onClose={closeCardDetailHandler}
+        />
+      )}
+    </>
   );
 }
